3e6a6b6ac18f65ab7d73ed4fbf3f0287a131b095
girishandroid <girish@whitebird.co.in>
2018-05-30 14:03:07 +0530

    Submit Databse Url Changes

--- Admin/application/config/config.php
+++ Admin/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/surfcity.in/Admin/';
+$config['base_url'] = 'http://surfcity.whitebird.co.in/Admin/';
 
 /*
 |--------------------------------------------------------------------------
